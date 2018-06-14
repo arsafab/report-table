0959d09262e9981c584c1f1392376ca0533b76ab
arsafab <dmitriibely@gmail.com>
2018-06-14 19:08:09 +0300

    create upload function in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ExcelToJsonService } from './services';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public result: any;
+
+  constructor(private excelToJson: ExcelToJsonService) {}
+
   public fileUpload(event): void {
     const fileList: FileList = event.target.files;
-    console.log(fileList);
+
+    this.excelToJson
+      .processFileToJson({}, fileList[0])
+      .subscribe(data => {
+        this.result = data;
+        console.log(this.result);
+      });
   }
 }
