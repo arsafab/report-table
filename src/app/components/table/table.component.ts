5c2af88a71386f52d0af0b35c56f160271e0d6a6
arsafab <dmitriibely@gmail.com>
2018-06-16 14:35:10 +0300

    fill weekends in object rows

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { IGroup, IObject } from '../../models';
 
 @Component({
@@ -6,11 +6,19 @@ import { IGroup, IObject } from '../../models';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent {
+export class TableComponent implements OnChanges {
   @Input() public group: IGroup;
   @Input() public date: Date;
 
+  public weekends: number[] = [];
+
   constructor() {}
+
+  public ngOnChanges(): void {
+    if (this.date && this.group) {
+      this.fillDefaultWeekends();
+    }
+  }
 
   public trackByFn(index: number): number {
     return index;
@@ -28,6 +36,22 @@ export class TableComponent {
     const year = this.date.getFullYear();
     const month = this.date.getMonth();
     const date = new Date(year, month, index + 1);
+
     return date.getDay() === 0 ? 7 : date.getDay();
+  }
+
+  public chooseWeekends(index: number): void {
+    this.weekends.includes(index)
+      ? this.weekends.splice(this.weekends.indexOf(index), 1)
+      : this.weekends.push(index);
+  }
+
+  private fillDefaultWeekends(): void {
+    (this.group.objects[0].fields as number[])
+        .forEach((item, i) => {
+          if (this.getDayNumber(i) === 7 || this.getDayNumber(i) === 6) {
+            this.weekends.push(i);
+          }
+        });
   }
 }
