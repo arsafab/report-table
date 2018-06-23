ffdce858fe8d7ef1fd00b331fd5341bdd099f902
arsafab <dmitriibely@gmail.com>
2018-06-23 18:20:46 +0300

    set excel props and save xls

--- src/app/services/js-to-excel.service.ts
+++ src/app/services/js-to-excel.service.ts
@@ -1,14 +1,32 @@
 import { Injectable } from '@angular/core';
 import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+import { IGroup } from '../models';
 
 @Injectable()
 export class JsToExcelService {
+  private workbook = XLSX.utils.book_new();
 
-  public generateReport(): void {
-    console.log(1);
+  public generateReport(group: IGroup): void {
+    this.setWorkbookProps(group.name);
+    console.log(group);
+    this.downloadExcel(group.name);
   }
 
-  downloadExcel(): void {
-    // FileSaver.saveAs(new Blob([this.s2ab(this.generateExcelFile())], { type: 'application/octet-stream' }), 'test.xlsx');
+  private setWorkbookProps(title: string): void {
+    this.workbook.Props = {
+      Title: title,
+      Subject: title,
+      Author: 'Roman Bely',
+      CreatedDate: new Date()
+    };
+  }
+
+  private downloadExcel(title: string): void {
+    FileSaver.saveAs(
+      new Blob([this.workbook],
+      { type: 'application/vnd.ms-excel;charset=charset=utf-8' }),
+      `${title}.xls`
+    );
   }
 }
