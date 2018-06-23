79a2996e6a40d1e1dbc587d81c216e7631f55d4f
arsafab <dmitriibely@gmail.com>
2018-06-23 19:23:42 +0300

    create arrayBuffer for excel export

--- src/app/services/js-to-excel.service.ts
+++ src/app/services/js-to-excel.service.ts
@@ -1,32 +1,65 @@
 import { Injectable } from '@angular/core';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
-import { IGroup } from '../models';
+import { IGroup, IObject } from '../models';
 
 @Injectable()
 export class JsToExcelService {
   private workbook = XLSX.utils.book_new();
+  private title: string;
+  private table: any;
 
   public generateReport(group: IGroup): void {
-    this.setWorkbookProps(group.name);
+    this.title = group.name;
+    this.setWorkbookProps();
+    this.generateTable(group.objects);
     console.log(group);
-    this.downloadExcel(group.name);
+    this.downloadExcel();
   }
 
-  private setWorkbookProps(title: string): void {
+  private setWorkbookProps(): void {
     this.workbook.Props = {
-      Title: title,
-      Subject: title,
+      Title: this.title,
+      Subject: this.title,
       Author: 'Roman Bely',
       CreatedDate: new Date()
     };
   }
 
-  private downloadExcel(title: string): void {
+  private generateTable(objects: IObject[]): void {
+    this.table = objects.map((item, index) => {
+      return [
+        index + 1,
+        item.name,
+        item.rate,
+        ...item.fields
+      ];
+    });
+  }
+
+  private generateExcelFile(): any {
+    this.workbook.SheetNames.push(this.title);
+    const worksheet = XLSX.utils.aoa_to_sheet(this.table);
+    this.workbook.Sheets[this.title] = worksheet;
+    return XLSX.write(this.workbook, { bookType: 'xlsx', type: 'binary' , cellStyles: true });
+  }
+
+  private s2ab(data: any): ArrayBuffer {
+    const buffer = new ArrayBuffer(data.length);
+    const view = new Uint8Array(buffer);
+    for (let i = 0; i !== data.length; ++i) {
+      // tslint:disable-next-line
+      view[i] = data.charCodeAt(i) & 0xFF;
+    }
+
+    return buffer;
+  }
+
+  private downloadExcel(): void {
     FileSaver.saveAs(
-      new Blob([this.workbook],
+      new Blob([this.s2ab(this.generateExcelFile())],
       { type: 'application/vnd.ms-excel;charset=charset=utf-8' }),
-      `${title}.xls`
+      `${this.title}.xls`
     );
   }
 }
